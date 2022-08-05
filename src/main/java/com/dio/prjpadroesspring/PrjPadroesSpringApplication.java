b6e49571ed1db00bc9619af4a13c9afc53e896e6
Eklezyarez <eklezyarez@gmail.com>
2022-08-05 20:29:43 -0300

    first commit

---
+++ src/main/java/com/dio/prjpadroesspring/PrjPadroesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dio.prjpadroesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjPadroesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjPadroesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/prjpadroesspring/PrjPadroesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.prjpadroesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjPadroesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
